6eb3ba78c196749e57ecee03ea147575332caead
mbg <michael.gale@syngate.co.uk>
2015-07-30 00:09:10 +0100

    Initial code commit

---
+++ DAL/EtilicContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etilic.Core.DAL
+{
+    public class EtilicContext : DbContext
+    {
+        #region Properties
+        public DbSet<Person> People
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// 
+        /// </summary>
+        public EtilicContext()
+            : base("EtilicContext")
+        {
+        }
+        #endregion
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.RegisterEntityType()
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etilic.Core
+{
+    /// <summary>
+    /// Represents a person.
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Gets or sets the globally unique ID of this person.
+        /// </summary>
+        public Guid ID
+        {
+            get;
+            set;
+        }
+    }
+}
